0400c5e18833d701cb166284bfae2354df9b4046
Mutarov <129605231+Mutarov@users.noreply.github.com>
2025-03-16 01:13:28 +0300

    livcsll v0.2

--- main.c
+++ main.c
@@ -1,15 +1,9 @@
 #include "livcsll.h"
 
 int main(void) {
-    INFO("PROGRAMM STARTED");
-    WARN("CMAKE NOT FOUND");
-    SUCCESS("TEST PASSED");
-    ERROR("%s", "BUILD FAILED");
-    sleep(1);
-    if (has_color_support() == 1) {
-        SUCCESS("has_color_support(): %d", has_color_support());
-    } else {
-        ERROR("has_color_support(): %d", has_color_support());
-    }
+    L_INFO("PROGRAMM STARTED");
+    L_WARN("CMAKE NOT FOUND");
+    L_SUCCESS("TEST PASSED");
+    L_ERROR("%s", "BUILD FAILED");
     return 0;
 }
